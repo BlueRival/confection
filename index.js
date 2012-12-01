37d3f9e60e78b9c111a0c4f3124a367804d8f25f
Anthony Hildoer <anthony@hildoersystems.com>
2012-12-01 09:49:30 -0700

    - initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+require( 'core-extensions' );
+var express = require( 'express' )();
+var storage = require( './storage' );
+var core = require( './lib/core' );
+var running = false;
+
+module.exports.run = function ( conf ) {
+
+	if ( running ) {
+		console.error( 'confection service already running' );
+		return;
+	}
+	running = true;
+
+	conf = {
+		server:  {
+			port: 8080
+		},
+		storage: {
+			host: 'localhost',
+			port: 6379
+		}
+	}.mixin( conf );
+
+	storage.init( conf.storage, function ( err ) {
+
+			if ( err ) {
+				console.error( 'could not initialize storage: ' + err );
+				process.exit( 1 );
+			}
+			else {
+				core.init( {
+					express: express,
+					storage: storage
+				}, function ( err ) {
+					if ( err ) {
+						console.error( 'could not start up core' );
+						process.exit( 1 );
+					}
+					else {
+						console.log( 'listening' );
+						express.listen( conf.server.port );
+					}
+				} );
+			}
+
+		}
+	);
+
+};
---
+++ storage/index.js
@@ -0,0 +1,211 @@
+"use strict";
+
+/*
+
+ Storage is a light wrapper around redis. It was implemented this way to make sure storage is modular and can be upgraded
+ later if other storage options are desired.
+
+ */
+
+var crypto = require( 'crypto' );
+var redis = require( 'redis' );
+var client = null;
+var clientConnectErr = null;
+var initCallbacks = [];
+
+module.exports.init = function ( params, callback ) {
+
+	if ( typeof params === 'function' ) {
+		callback = params;
+		params = {};
+	}
+
+	initCallbacks.push( callback );
+
+	function flushCallback( callback, err ) {
+		process.nextTick( function () {
+			executeCallback( callback, err );
+		} );
+	}
+
+	function flushInitCallbacks() {
+		while ( initCallbacks.length > 0 ) {
+			flushCallback( initCallbacks.shift(), clientConnectErr );
+		}
+	}
+
+	if ( !client ) {
+
+		params = {
+			host:    "localhost",
+			port:    6379,
+			options: {}
+		}.mixin( params );
+
+		client = redis.createClient( params.port, params.host, params.options );
+
+		var errorHandler = function () {
+			client = null;
+			clientConnectErr = 'failed to connect';
+			client.removeListener( 'connect', connectHandler );
+			flushInitCallbacks();
+		};
+
+		var connectHandler = function () {
+			clientConnectErr = null;
+			client.removeListener( 'error', errorHandler );
+			flushInitCallbacks();
+		};
+
+		client.on( 'connect', connectHandler );
+		client.on( 'error', errorHandler );
+
+	}
+	else {
+		flushInitCallbacks();
+	}
+
+};
+
+module.exports.getConf = function ( key, callback ) {
+	getRaw( key, 'conf', callback );
+};
+
+module.exports.setConf = function ( key, conf, callback ) {
+	setRaw( key, 'conf', conf, callback );
+};
+
+module.exports.delConf = function ( key, callback ) {
+	delRaw( key, 'conf', callback );
+};
+
+module.exports.isAuthorized = function ( key, callback ) {
+
+	getRaw( key, 'auth', function ( err, value ) {
+		executeCallback( callback, ( value && !err ) );
+	} );
+
+};
+
+module.exports.generateAuthKey = function ( callback ) {
+
+	var hash = crypto.createHash( 'sha1' );
+	hash.update( new Date().getTime() + "|" + Math.random() );
+	hash = hash.digest( 'hex' );
+
+	setRaw( hash, 'auth', true, function ( err ) {
+		if ( err ) {
+			executeCallback( callback, null );
+		}
+		else {
+			executeCallback( callback, hash );
+		}
+	} );
+
+};
+
+module.exports.delAuthKey = function ( key, callback ) {
+	delRaw( key, 'auth', callback );
+};
+
+function delRaw( key, type, callback ) {
+
+	checkReady();
+
+	client.del( encodeKey( key, type ), function ( err ) {
+
+		executeCallback( callback, err ? "could not delete " + type : null );
+
+	} );
+
+}
+
+function getRaw( key, type, callback ) {
+
+	checkReady();
+
+	client.get( encodeKey( key, type ), function ( err, data ) {
+
+		data = decodeData( data );
+
+		if ( err || !data ) {
+			executeCallback( callback, "could not retrieve " + type, null );
+		}
+		else {
+			executeCallback( callback, null, data );
+		}
+
+	} );
+
+}
+
+function setRaw( key, type, data, callback ) {
+
+	checkReady();
+
+	data = encodeData( data );
+
+	if ( !data ) {
+		executeCallback( callback, type + " could not be serialized", null );
+		return;
+	}
+
+	client.set( encodeKey( key, type ), data, function ( err, data ) {
+
+		if ( err || !data ) {
+			executeCallback( callback, "could not store " + type );
+		}
+		else {
+			executeCallback( callback, null );
+		}
+	} );
+
+}
+
+function executeCallback() {
+	var args = Array.prototype.slice.call( arguments );
+
+	if ( typeof args[0] === 'function' ) {
+		args[0].apply( {}, args.slice( 1 ) );
+	}
+}
+
+function encodeKey( key, type ) {
+
+	return type + ":" + key;
+
+}
+
+function encodeData( data ) {
+
+	try {
+		return JSON.stringify( data );
+	}
+	catch ( e ) {
+		return null;
+	}
+
+}
+
+function decodeData( data ) {
+
+	if ( !data ) {
+		return null;
+	}
+
+	try {
+		return JSON.parse( data );
+	}
+	catch ( e ) {
+		return null;
+	}
+
+}
+
+function checkReady() {
+
+	if ( !client ) {
+		throw "storage has not been initialized";
+	}
+
+}
